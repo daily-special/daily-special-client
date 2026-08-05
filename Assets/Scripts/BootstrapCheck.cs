310c2093e734bea2af2add2d3d4eb6c4ecec4c25
Jaebeom LEe <8000gam@gmail.com>
2026-08-06 01:23:21 +0900

    chore: Unity 프로젝트 초기 설정

---
+++ Assets/Scripts/BootstrapCheck.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class BootstrapCheck : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("오늘의 정식 Unity 개발 환경이 정상입니다.");
+    }
+
+}
